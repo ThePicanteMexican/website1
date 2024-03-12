81f8e0bd84d265dd672719d6adaa6a7910893d3a
ThePicanteMexican <Jaydencruz41108@gmail.com>
2024-03-12 21:32:01 +0000

    add

---
+++ js/landingPage.js
@@ -0,0 +1,21 @@
+toggle = document.getElementById('toggle')
+open = document.getElementById('open')
+modal = document.getElementById('modal')
+
+toggle.addEventListener('click', () => {
+    document.body.classList.toggle('show-nav')
+})
+
+open.addEventListener('click', () => {
+    modal.classList.add('show-modal')
+})
+
+close.addEventListener('click', () => {
+    modal.classList.remove('show-modal')
+})
+
+window.addEventListener('click', (e) => {
+    if(e.target == modal) {
+        modal.classList.remove('show-modal')
+    }
+})
